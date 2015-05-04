baa97b64056d980ff116c6f344df848502364b74
mdamis <mdamis@users.noreply.github.com>
2015-05-04 22:07:59 +0200

    Add command line support

--- src/upem/jarret/server/Server.java
+++ src/upem/jarret/server/Server.java
@@ -6,12 +6,34 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Server {
 	private final ServerSocketChannel ssc;
 	private final Selector selector;
 	private final Set<SelectionKey> selectedKeys;
+
+	private final Thread consoleThread = new Thread(() -> {
+		try (Scanner scanner = new Scanner(System.in)) {
+			while (scanner.hasNextLine()) {
+				switch (scanner.nextLine()) {
+				case "SHUTDOWN":
+					shutdown();
+					return;
+				case "SHUTDOWN NOW":
+					shutdownNow();
+					return;
+				case "INFO":
+					info();
+					break;
+				default:
+					System.out.println("WRONG COMMAND");
+					break;
+				}
+			}
+		}
+	});
 
 	public Server(int port) throws IOException {
 		ssc = ServerSocketChannel.open();
@@ -20,7 +42,40 @@ public class Server {
 		selectedKeys = selector.selectedKeys();
 	}
 
+	private void info() {
+		System.out.println("INFO");
+		// TODO
+	}
+
+	private void shutdown() {
+		System.out.println("SHUTDOWN");
+		try {
+			ssc.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void shutdownNow() {
+		System.out.println("SHUTDOWN NOW");
+		try {
+			ssc.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		for (SelectionKey key : selectedKeys) {
+			try {
+				close(key);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	public void launch() throws IOException {
+		consoleThread.start();
+
 		ssc.configureBlocking(false);
 		ssc.register(selector, SelectionKey.OP_ACCEPT);
 		Set<SelectionKey> selectedKeys = selector.selectedKeys();
@@ -35,10 +90,10 @@ public class Server {
 		for (SelectionKey key : selectedKeys) {
 			if (key.isValid() && key.isAcceptable()) {
 				try {
-	                doAccept(key);
-                } catch (IOException e) {
-	                e.printStackTrace();
-                }
+					doAccept(key);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 			if (key.isValid() && key.isWritable()) {
 				doWrite(key);
@@ -51,7 +106,7 @@ public class Server {
 
 	private void doAccept(SelectionKey key) throws IOException {
 		SocketChannel sc = ssc.accept();
-		if(sc == null) {
+		if (sc == null) {
 			return;
 		}
 		sc.configureBlocking(false);
@@ -67,13 +122,13 @@ public class Server {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	private void close(SelectionKey key) throws IOException {
 		key.channel().close();
 	}
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-	    new Server(Integer.parseInt(args[0])).launch();
-    }
+		new Server(Integer.parseInt(args[0])).launch();
+	}
 
 }
